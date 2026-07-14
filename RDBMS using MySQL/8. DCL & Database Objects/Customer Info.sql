6141289cc81e61959950c0ccbe687035f70e5417
mani-kartik <mani.kartik944@gmail.com>
2026-07-15 01:13:49 +0530

    Update Customer Info.sql

--- RDBMS using MySQL/8. DCL & Database Objects/Customer Info.sql
+++ RDBMS using MySQL/8. DCL & Database Objects/Customer Info.sql
@@ -1,11 +1,11 @@
 create view Customer_Info as 
 select 
     c.C_first_name, 
-    c. Phoneno, 
+    c.Phoneno, 
     c.City, 
-    b. Total_amount as Total_amount 
-from Customer_master c 
-join Enquiry_master e on c.Cust_id=e.Cust_id 
-join Booking_master b on e. Enquiry_id=b.Enquiry_id 
-where Total_amount<60000 
+    b.Total_amount
+from Customer_Master c 
+join Enquiry_Master e on c.Cust_id = e.Cust_id 
+join Booking_Master b on e.Enquiry_id = b.Enquiry_id 
+where b.Total_amount < 60000 
 order by c.C_first_name;
